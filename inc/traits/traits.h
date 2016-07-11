62b65a32d9d07a75bc4979b4c1fe8ea23e72e75f
Tom Bishop <tom.smart.bishop@gmail.com>
2016-07-11 22:20:56 +0900

    Added more shortcuts with the using directive - shorter type traits;

--- inc/traits/traits.h
+++ inc/traits/traits.h
@@ -11,71 +11,81 @@ namespace avl {
 	/// \defgroup Type Traits
 	/// @{
     
+    /// @name Shortcuts
+	/// @{
+        
     template<typename _Tp>
     using cmp_t = s::remove_extent_t<_Tp>;
     
     template<typename _Tp>
     using is_sc = typename s::is_arithmetic<_Tp>::value;
+    
+    using true_t = s::true_type;
+    using false_t = s::false_type;
+    
+    template<typename _Tp>
+    using true_t_if = s::enable_if_t< _Tp, true_t>;
 	
+    /// @}
     /// @name Is Euclidean Vector
 	/// @{
 
 	template<typename>
-	struct is_eu_vec : public s::false_type { };
+	struct is_eu_vec : public false_t { };
 
 	template<typename _Tp, s::size_t _Size>
-	struct is_eu_vec<_Tp[_Size]> : s::enable_if_t< is_sc< cmp_t<_Tp> >, s::true_type > {};
+	struct is_eu_vec<_Tp[_Size]> : true_t_if< is_sc< cmp_t<_Tp> > > {};
 
 	template<typename _Tp>
-	struct is_eu_vec<_Tp[]> : s::enable_if_t< is_sc< cmp_t<_Tp> >, s::true_type > {};
+	struct is_eu_vec<_Tp[]> : true_t_if< is_sc< cmp_t<_Tp> > > {};
 
 	/// @}
 	/// @name Is 2D Euclidean Vector
 	/// @{
 
 	template<typename>
-	struct is_eu_vec2 : public s::false_type {};
+	struct is_eu_vec2 : public false_t {};
 
 	template<typename _Tp, s::size_t _Size>
-	struct is_eu_vec2<_Tp[_Size]> : public s::false_type {};
+	struct is_eu_vec2<_Tp[_Size]> : public false_t {};
 
 	template<typename _Tp>
-	struct is_eu_vec2<_Tp[2]> : s::enable_if_t< is_sc< cmp_t<_Tp> >, s::true_type > {};
+	struct is_eu_vec2<_Tp[2]> : true_t_if< is_sc< cmp_t<_Tp> > > {};
 
 	template<typename _Tp>
-	struct is_eu_vec2<_Tp[]> : s::enable_if_t< _Tp::dim==2 && is_sc< cmp_t<_Tp> >, s::true_type > {};
+	struct is_eu_vec2<_Tp[]> : true_t_if< _Tp::dim==2 && is_sc< cmp_t<_Tp> > > {};
 
 	/// @}
 	/// @name Is 3D Euclidean Vector
 	/// @{
 
 	template<typename>
-	struct is_eu_vec3 : public s::false_type {};
+	struct is_eu_vec3 : public false_t {};
 
 	template<typename _Tp, s::size_t _Size>
-	struct is_eu_vec3<_Tp[_Size]> : public s::false_type {};
+	struct is_eu_vec3<_Tp[_Size]> : public false_t {};
 
 	template<typename _Tp>
-	struct is_eu_vec3<_Tp[3]> : s::enable_if_t< is_sc< cmp_t<_Tp> >, s::true_type > {};
+	struct is_eu_vec3<_Tp[3]> : true_t_if< is_sc< cmp_t<_Tp> > > {};
 
 	template<typename _Tp>
-	struct is_eu_vec3<_Tp[]> : s::enable_if_t< _Tp::dim==3 && is_sc< cmp_t<_Tp> >, s::true_type > {};
+	struct is_eu_vec3<_Tp[]> : true_t_if< _Tp::dim==3 && is_sc< cmp_t<_Tp> > > {};
 
 	/// @}
 	/// @name Is 4D Euclidean Vector
 	/// @{	template<typename>
 
 	template<typename>
-	struct is_eu_vec4 : public s::false_type {};
+	struct is_eu_vec4 : public false_t {};
 
 	template<typename _Tp, s::size_t _Size>
-	struct is_eu_vec4<_Tp[_Size]> : public s::false_type {};
+	struct is_eu_vec4<_Tp[_Size]> : public false_t {};
 
 	template<typename _Tp>
-	struct is_eu_vec4<_Tp[4]> : s::enable_if_t< is_sc< cmp_t<_Tp> >, s::true_type > {};
+	struct is_eu_vec4<_Tp[4]> : true_t_if< is_sc< cmp_t<_Tp> > > {};
 
 	template<typename _Tp>
-	struct is_eu_vec4<_Tp[]> : s::enable_if_t< _Tp::dim==4 && is_sc< cmp_t<_Tp> >, s::true_type > {};
+	struct is_eu_vec4<_Tp[]> : true_t_if< _Tp::dim==4 && is_sc< cmp_t<_Tp> > > {};
 
 	/// @}
 
@@ -99,24 +109,24 @@ namespace avl {
 	/// \defgroup Concepts
 	/// @{
 
-	template <typename S>
-	concept bool sc = s::is_arithmetic<S>::value;
+	template <typename _Tp>
+	concept bool sc = is_sc<_Tp>;
 
-	template <typename V>
-	concept bool v = is_eu_vec<V>::value; //implicitly also checks s::is_array
+	template <typename _Tp>
+	concept bool v = is_eu_vec<_Tp>::value; //implicitly also checks s::is_array
 
-	template <typename V>
-	concept bool v2 = is_eu_vec2<V>::value; //implicitly also checks s::is_array
+	template <typename _Tp>
+	concept bool v2 = is_eu_vec2<_Tp>::value; //implicitly also checks s::is_array
 
-	template <typename V>
-	concept bool v3 = is_eu_vec3<V>::value; //implicitly also checks s::is_array
+	template <typename _Tp>
+	concept bool v3 = is_eu_vec3<_Tp>::value; //implicitly also checks s::is_array
 
-	template <typename V>
-	concept bool v4 = is_eu_vec4<V>::value; //implicitly also checks s::is_array
+	template <typename _Tp>
+	concept bool v4 = is_eu_vec4<_Tp>::value; //implicitly also checks s::is_array
 
 	/// Vector component type equals scalar type
-	template <typename V, typename S>
-	struct vec_cmp_eq_s : s::is_same<s::add_const_t< cmp_t< s::remove_reference_t<V> > >, S> {};
+	template <typename _Vec, typename _Sclr>
+	struct vec_cmp_eq_s : s::is_same<s::add_const_t< cmp_t< s::remove_reference_t<_Vec> > >, _Sclr> {};
 
 	/// @}
 }
