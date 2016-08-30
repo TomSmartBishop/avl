994029d18473db3c88fb79bf5819c35c0a6c8201
Tom Bishop <tom.smart.bishop@gmail.com>
2016-08-30 11:48:16 +0900

    Added a lost of const prefixes; Modified func to allow multiple test_case(s); Added if_then_else, deg_to_rad, getRndFloatPos, getRndFloatNeg macro; Refactored angle_xx functions (and added test cases);

--- inc/helper/const.h
+++ inc/helper/const.h
@@ -9,23 +9,23 @@ namespace avl
 	template<typename P>
 	struct cnst {
 		//actually these constexpr do not require a struct according to c++14, but through compiler limitations (msvc) we stick to this format
-		static constexpr P pi		{ M_PI };
-		static constexpr P piH		{ M_PI_2 };
-		static constexpr P piQ		{ M_PI_4 };
-		static constexpr P piInv	{ M_1_PI };
+		static constexpr P pi			{ M_PI };
+		static constexpr P pi_2			{ M_PI_2 };
+		static constexpr P pi_4			{ M_PI_4 };
+		static constexpr P pi_inv		{ M_1_PI };
 		
-		static constexpr P toDeg	{ 180.0 * piInv };
-		static constexpr P toRad	{ pi / 180.0 };
+		static constexpr P to_deg		{ 180.0 * piInv };
+		static constexpr P to_rad		{ pi / 180.0 };
 		
-		static constexpr P sqrt2	{ M_SQRT2 };
-		static constexpr P sqrt2Inv	{ M_SQRT1_2 };
+		static constexpr P sqrt2		{ M_SQRT2 };
+		static constexpr P sqrt2_inv	{ M_SQRT1_2 };
 		
-		static constexpr P one		{  1 };
-		static constexpr P minus_one{ -1 };
-		static constexpr P zero		{  0 };
+		static constexpr P one			{  1 };
+		static constexpr P minus_one	{ -1 };
+		static constexpr P zero			{  0 };
 		
-		static constexpr P smlEpsln	{ 0.000001 };
-		static constexpr P bigEpsln	{ 0.001 };
+		static constexpr P sml_epsln	{ 0.000001 };
+		static constexpr P big_epsln	{ 0.001 };
 	};
 	
 #endif // AVL_CONST_H
